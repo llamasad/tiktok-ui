3634f3bcc22a284175ef91a4b9c7aa471906408e
llamasad <datdo2712003@gmail.com>
2023-07-11 20:03:48 +0700

    97

--- src/components/Layout/Components/Header/index.js
+++ src/components/Layout/Components/Header/index.js
@@ -1,5 +1,13 @@
+import style from './Header.module.scss';
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(style);
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}></div>
+    </header>
+  );
 }
 
 export default Header;
